55595abef749e30146651292e5baa5a5044b09a2
Yeison Estiven Perea Córdoba <ye.perea09@ciaf.edu.co>
2025-03-25 08:51:26 -0500

    update files index.html and app.js

--- js/app.js
+++ js/app.js
@@ -2,19 +2,21 @@ document.getElementById("currentYear").textContent = "© " + new Date().getFullY
 async function getProducts() {
   try {
     const response = await fetch(
-      "https://api.escuelajs.co/api/v1/products?offset=0&limit=12"
+      "https://fakestoreapi.com/products?limit=12"
     );
     const products = await response.json();
+    console.log(products);
+
 
     products.forEach((product) => {
       container = document.getElementById("cards-container");
       const colDiv = document.createElement("div");
-      colDiv.classList.add("col");
+      colDiv.classList.add("col-12", "col-sm-6", "col-md-4", "col-lg-3", "d-flex");
 
       colDiv.innerHTML = `<div class="py-3">
-    <div class="card" style="width: 18rem">
-                  <img src="${product.images[2]}" class="card-img-top" alt="img-${product.title}" />
-                  <div class="card-body">
+    <div class="card h-100 d-flex flex-column w-100" style="width: 18rem">
+                  <img src="${product.image}" class="card-img-top" style="height: 200px; object-fit: cover;" alt="img-${product.title}" />
+                  <div class="card-body d-flex flex-column">
                     <h5 class="card-title">${product.title}</h5>
                     <p class="card-text">
                       ${product.description.split(" ").slice(0, 10).join(" ")}
